4d497099942c59b95e19b2127b20a92f45ec67d6
Patrick Levesque <patrick@paralel.ca>
2024-02-19 19:18:28 -0500

    Update code to use config and no terminal
    Uses child_process to call “herd restart” and log output in console

--- src/extension.ts
+++ src/extension.ts
@@ -1,62 +1,51 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import * as fs from 'fs';
+import * as os from 'os';
 import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "toggle-xdebug" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	let disposable = vscode.commands.registerCommand('toggle-xdebug.helloWorld', () => {
-// 		// The code you place here will be executed every time your command is executed
-// 		// Display a message box to the user
-// 		vscode.window.showInformationMessage('Hello PARALEL New!');
-// 	});
-
-// 	context.subscriptions.push(disposable);”
-// }
-
 export function activate(context: vscode.ExtensionContext) {
-    let enableDisposable = vscode.commands.registerCommand('toggle-xdebug.enableXdebug', async () => {
-        let phpVersion = await vscode.window.showQuickPick(['8.2', '8.3'], { placeHolder: 'Select PHP version' });
+    let enableDisposable = vscode.commands.registerCommand('herd-xdebug-toggler.enableXdebug', async () => {
+		// Get the PHP versions from the configuration file
+		let config = vscode.workspace.getConfiguration('herdXdebugToggler');
+		let phpVersions = config.get('phpVersions');
+		
+		// Check if phpVersions is an array
+		if (!Array.isArray(phpVersions)) {
+			throw new Error('PHP versions are missing or invalid in the configuration file. Please check the README file for more information.');
+		}
+
+		let phpVersion = await vscode.window.showQuickPick(phpVersions, { placeHolder: 'Select PHP version' });
+		
 		if (!phpVersion) {
 			vscode.window.showErrorMessage('PHP version is required');
 			return;
 		}
 
 		// Remove the dot from the version
-		phpVersion = phpVersion.replace('.', '');
+		let phpVersionShort = phpVersion.replace('.', '');
 		
 		// Determine the path to the php.ini file
-		let phpIniPath = '/Users/plevesque/Library/Application Support/Herd/config/php/' + phpVersion + '/php.ini';
+		let homeDir = os.homedir();
+		let phpIniPath = path.join(homeDir, 'Library/Application Support/Herd/config/php/', phpVersionShort, 'php.ini');
 		
 		// The lines to check
-		let configLines = [
-			'xdebug.mode=debug,develop',
-			'xdebug.start_with_request=yes',
-			'xdebug.start_upon_error=yes'
-		];
+		// let configLines = [
+		// 	'xdebug.mode=debug,develop',
+		// 	'xdebug.start_with_request=yes',
+		// 	'xdebug.start_upon_error=yes'
+		// ];
 
-		let extensionLine = 'zend_extension=/Applications/Herd.app/Contents/Resources/xdebug/xdebug-' + phpVersion + '-arm64.so';
+		let extensionLine = 'zend_extension=/Applications/Herd.app/Contents/Resources/xdebug/xdebug-' + phpVersionShort + '-arm64.so';
 
         // Read the file
         fs.readFile(phpIniPath, 'utf8', (err, data) => {
             if (err) {
-                vscode.window.showErrorMessage('Could not find php.ini file');
+                vscode.window.showErrorMessage('Could not find php.ini file for PHP ' + phpVersion);
                 return;
             }
 
 			// Check if the extension is already enabled
 			if (!data.includes(';' + extensionLine) && data.includes(extensionLine)) {
-			// if (!data.includes(';zend_extension=/Applications/Herd.app/Contents/Resources/xdebug/xdebug-82-arm64.so') && data.includes('zend_extension=/Applications/Herd.app/Contents/Resources/xdebug/xdebug-82-arm64.so')) {
 				vscode.window.showInformationMessage('Xdebug  extension is already enabled');
 				return;
 			}
@@ -78,31 +67,59 @@ export function activate(context: vscode.ExtensionContext) {
 
                 vscode.window.showInformationMessage('Successfully enabled Xdebug extension');
 
-				// // Create a new terminal
-				// let terminal = vscode.window.createTerminal();
+				const { exec } = require('child_process');
 
-				// // Execute the "herd restart" command
-				// terminal.sendText('herd restart');
-			
-				// // Show the terminal
-				// terminal.show();
+				exec('herd restart', (error: any, stdout: any, stderr: any) => {
+					console.log('Herd Xdebug Toggler -- Restarting Herd');
+
+					if (error) {
+						console.error(`Execution error: ${error}`);
+						return;
+					} else {
+						console.log(stdout);
+					}
+				});
             });
         });
     });
 
-    let disableDisposable = vscode.commands.registerCommand('toggle-xdebug.disableXdebug', () => {
-        // The code you place here will be executed every time your command is executed
+    let disableDisposable = vscode.commands.registerCommand('herd-xdebug-toggler.disableXdebug', async () => {
+		// Get the PHP versions from the configuration file
+		let config = vscode.workspace.getConfiguration('herdXdebugToggler');
+		let phpVersions = config.get('phpVersions');
+		
+		// Check if phpVersions is an array
+		if (!Array.isArray(phpVersions)) {
+			throw new Error('PHP versions are missing or invalid in the configuration file. Please check the README file for more information.');
+		}
 
-        // Determine the path to the php.ini file
-		// let phpIniPath = path.join(vscode.workspace., 'php.ini');
-		let phpIniPath = '/Users/plevesque/Library/Application Support/Herd/config/php/82/php.ini';
+		let phpVersion = await vscode.window.showQuickPick(phpVersions, { placeHolder: 'Select PHP version' });
+		
+		if (!phpVersion) {
+			vscode.window.showErrorMessage('PHP version is required');
+			return;
+		}
 
-		let extensionLine = 'zend_extension=/Applications/Herd.app/Contents/Resources/xdebug/xdebug-82-arm64.so';
+		// Remove the dot from the version
+		let phpVersionShort = phpVersion.replace('.', '');
+		
+		// Determine the path to the php.ini file
+		let homeDir = os.homedir();
+		let phpIniPath = path.join(homeDir, 'Library/Application Support/Herd/config/php/', phpVersionShort, 'php.ini');
+		
+		// The lines to check
+		// let configLines = [
+		// 	'xdebug.mode=debug,develop',
+		// 	'xdebug.start_with_request=yes',
+		// 	'xdebug.start_upon_error=yes'
+		// ];
+
+		let extensionLine = 'zend_extension=/Applications/Herd.app/Contents/Resources/xdebug/xdebug-' + phpVersionShort + '-arm64.so';
 
         // Read the file
         fs.readFile(phpIniPath, 'utf8', (err, data) => {
             if (err) {
-                vscode.window.showErrorMessage('Could not find php.ini file');
+                vscode.window.showErrorMessage('Could not find php.ini file for PHP ' + phpVersion);
                 return;
             }
 
@@ -124,14 +141,19 @@ export function activate(context: vscode.ExtensionContext) {
 
                 vscode.window.showInformationMessage('Successfully disabled Xdebug extension');
 
-				// // Create a new terminal
-				// let terminal = vscode.window.createTerminal();
 
-				// // Execute the "herd restart" command
-				// terminal.sendText('herd restart');
-			
-				// // Show the terminal
-				// terminal.show();
+				const { exec } = require('child_process');
+
+				exec('herd restart', (error: any, stdout: any, stderr: any) => {
+					console.log('Herd Xdebug Toggler -- Restarting Herd');
+
+					if (error) {
+						console.error(`Execution error: ${error}`);
+						return;
+					} else {
+						console.log(stdout);
+					}
+				});
             });
         });
     });
